874f22ac85081e1cd0560342e01f674480f5a075
zanklia90-zankli <zanklia90@gmail.com>
2025-10-28 11:39:04 +0100

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import { AuthProvider } from './context/AuthContext.tsx';
 import { RequestProvider } from './context/RequestContext.tsx';
 import { VendorProvider } from './context/VendorContext.tsx';
